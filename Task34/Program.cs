f5ccdb3710ac1cff8ff1524df2f6399933f2ed14
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-30 21:33:26 +0700

    task 34 solved

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -11,15 +11,28 @@ int [] CreateArray(int size, int min, int max) // Метод для заполн
     Random rand = new Random();
     for (int i = 0; i < size; i++)
     {
-        array[i] = rand.Next(min, max+1)
+        array[i] = rand.Next(min, max+1);
     }
     return array;
 }
 
 string PrintArray(int [] array) // Метод для печати массива.
 {
-    return string.Join(", " array);
+    return string.Join(", ", array);
 }
 
-int array = CreateArray(4, 100, 999);
+int CheckArrayElements(int [] array) // Проверка на четность.
+{
+    int count = 0;
+    foreach(int el in array)
+    {
+       count += (el % 2 == 0) ? 1 : 0;
+    }
+    return count;
+}
+
+int [] array = CreateArray(4, 100, 999); // Диапазон положительных трехзначных чисел. 
 Console.WriteLine($"[{PrintArray(array)}]");
+
+int result = CheckArrayElements(array);
+Console.WriteLine($"This array contains {result} of even numbers.");
